77a3cd5a01c8ab71b829b8fb0e199e63c7830378
pyrsmk <dev@dreamysource.fr>
2014-02-24 21:11:33 +0100

    Improve CLI report

--- src/MiniSuite/Cli.php
+++ src/MiniSuite/Cli.php
@@ -13,10 +13,31 @@ use MiniSuite;
 class Cli extends MiniSuite{
 
 	/*
+		boolean $colors
+	*/
+	protected $colors=true;
+
+	/*
+		Disable ANSI colors (some environments don't support them, like Windows)
+
+		Return
+			MiniSuite\Cli
+	*/
+	public function disableAnsiColors(){
+		$this->colors=false;
+		return $this;
+	}
+
+	/*
 		Triggered before running tests
 	*/
 	protected function _beforeTests(){
-		echo "\n  \033[1;33m".$this->name."\n\n";
+		if($colors){
+			echo "\n  \033[1;33m$this->name\n\n";
+		}
+		else{
+			echo "\n  $this->name\n\n";
+		}
 	}
 
 	/*
@@ -33,7 +54,12 @@ class Cli extends MiniSuite{
 			string $message
 	*/
 	protected function _testPassed($message){
-		echo "      \033[0;32mPassed : $message\n";
+		if($colors){
+			echo "      \033[0;32m$message\n";
+		}
+		else{
+			echo "      Passed : $message\n";
+		}
 	}
 
 	/*
@@ -43,7 +69,12 @@ class Cli extends MiniSuite{
 			string $message
 	*/
 	protected function _testFailed($message){
-		echo "      \033[0;31mFailed : $message\n";
+		if($colors){
+			echo "      \033[0;31m$message\n";
+		}
+		else{
+			echo "      Failed : $message\n";
+		}
 	}
 
 }
