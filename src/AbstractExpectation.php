b05fe529530e9931f163cb1fa94431332dd79c46
pyrsmk <dev@dreamysource.fr>
2016-06-30 15:51:14 +0200

    Add better support for array and boolean values printing

--- src/AbstractExpectation.php
+++ src/AbstractExpectation.php
@@ -20,8 +20,15 @@ abstract class AbstractExpectation implements ExpectationInterface {
 		if(is_object($value)) {
 			return get_class($value);
 		}
-		if(is_array($value)) {
-			return 'Array['.count($value).']';
+		else if(is_array($value)) {
+			return '['.implode(', ', array_map(
+				function ($v, $k) { return sprintf("%s => %s", $k, $v); },
+				$value,
+				array_keys($value)
+			)).']';
+		}
+		else if(is_bool($value)) {
+			return $value ? 'true' : 'false';
 		}
 		else if(is_resource($value)) {
 			$type=get_resource_type($value);
